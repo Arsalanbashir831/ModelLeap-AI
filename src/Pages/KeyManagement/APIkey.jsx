1157c74e01f6ca1a1f4be6c0fe755b13a4920a23
hamza <muhammadhamzasalahuddin@gmail.com>
2024-09-29 16:19:34 +0500

    layout keys page

--- src/Pages/KeyManagement/APIkey.jsx
+++ src/Pages/KeyManagement/APIkey.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Header from "../../Components/Dashboard/Header";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import KeyGenerate from "../../Components/Dashboard/KeyGenerate";
 
 const APIkey = () => {
   return (
-    <>
-      <Box mt={8} p={"3"}>
+    <Box minH="100vh">
+      <Box mt={8} p={3}>
         <Header title={"API Key Management"} />
       </Box>
-      <Box>
+      <Box borderRadius="lg" p={3} width="100%" maxW="950px">
         <KeyGenerate />
       </Box>
-    </>
+    </Box>
   );
 };
 
